55bf1db900e236d8d7943ade6a0d289b6c60c1d1
Trupti100 <127935389+Trupti100@users.noreply.github.com>
2023-11-08 09:08:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Person class
 class Person {
   constructor(name, age) {
     this._name = name;
@@ -10,36 +9,29 @@ class Person {
   }
 
   set age(age) {
-    if (age >= 0) {
-      this._age = age;
-    } else {
-      console.error("Age cannot be negative.");
-    }
+    this._age = age;
   }
 }
 
-// Student class that extends Person
 class Student extends Person {
   study() {
-    console.log(`${this.name} is studying.`);
+    console.log(`${this.name} is studying`);
   }
 }
 
-// Teacher class that extends Person
 class Teacher extends Person {
   teach() {
-    console.log(`${this.name} is teaching.`);
+    console.log(`${this.name} is teaching`);
   }
 }
 
-// Example usage:
+// Example usage
 const person = new Person("John", 30);
-console.log(person.name); // Output: John
-person.age = 35; // Set the age
-console.log(person.age); // Output: 35
-
 const student = new Student("Alice", 20);
-student.study(); // Output: Alice is studying.
-
 const teacher = new Teacher("Mr. Smith", 40);
-teacher.teach(); // Output: Mr. Smith is teaching.
+
+console.log(person.name); // Output: John
+person.age = 35; // Setter example
+
+student.study(); // Output: Alice is studying
+teacher.teach(); // Output: Mr. Smith is teaching
